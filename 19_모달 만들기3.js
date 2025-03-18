4d45d2df5d61daa67388d131d166502d9607ded5
Boramigood <134863574+Boramigood@users.noreply.github.com>
2025-03-18 16:00:13 +0900

    19_모달 만들기3.js

--- 19_모달 만들기3.js
+++ 19_모달 만들기3.js
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" />
-    <title>Document</title>
-  </head>
-  <body>
-    <div class="time hour">00</div>
-    <div class="divider">:</div>
-    <div class="time min">00</div>
-    <div class="divider">:</div>
-    <div class="time sec">00</div>
+const openbutton = document.querySelector('.open'); //class니까 .붙히기기
+const container = document.querySelector('.container');
+const closebutton = document.querySelector('.close');
 
-    <script src="./index.js"></script>
-  </body>
-</html>
+openbutton.addEventListener('click', () => {
+  container.style.display = 'flex'; //style에 display가 있는거임
+  openbutton.style.display = 'none';
+});
+
+closebutton.addEventListener('click', () => {
+  container.style.display = 'none';
+  openbutton.style.display = 'block';
+});
