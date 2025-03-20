ee0edf799a8d18d5bf47d11d1a978454cec25f1e
Boramigood <134863574+Boramigood@users.noreply.github.com>
2025-03-20 12:53:27 +0900

    20_To Do List4.js

--- 20_To Do List4.js
+++ 20_To Do List4.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   if (input.value !== '') {
-    li = document.createElement('li'); //const안써도 돌아감??
+    li = document.createElement('li'); //const안써도 돌아감?? > 정의 안할시 var취급(const나 let 사용할 것)
     li.innerText = input.value; //innerText!(가상이라고 해도 li자체는 <li></li>임)
     ul.appendChild(li); //appenChild는 ''없음
 
